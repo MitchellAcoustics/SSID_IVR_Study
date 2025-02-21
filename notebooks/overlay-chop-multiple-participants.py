078a265898cfa1c346421940a36a78835690bcb3
yuqiliang <41510849+yuqiliang@users.noreply.github.com>
2025-02-21 12:58:19 +0000

    experiment to merge to draft

--- notebooks/overlay-chop-multiple-participants.py
+++ notebooks/overlay-chop-multiple-participants.py
@@ -1,7 +1,3 @@
-# Yuqi to write a for loop to perform the chop and overlay video processing for multiple participants.
-# Pull together the code from `single-video-processing.ipynb`
-
-# Suggest running this with: `caffeinate -i uv run overlay-chop-multiple-participants.py`
 
 """
 Video Processing for Eye Tracking Analysis
